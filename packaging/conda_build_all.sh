6eb6db8a46dcd06cb3a2c5d26787291ae9923689
Jon Mease <jon.mease@gmail.com>
2019-03-09 10:47:36 -0500

    Add packaging directory for conda recipes of dependencies

---
+++ packaging/conda_build_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+conda build flask-compress/
+conda build dash-renderer/
+conda build dash/
+conda build dash-html-components/
+conda build dash-core-components/
+conda build dash-table/
+conda build dash-daq/
