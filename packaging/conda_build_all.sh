d2bf7aa1f78ca6866d40895ad138700c5b7e1f02
Jon Mease <jon.mease@gmail.com>
2019-03-24 16:54:18 -0400

    Remove packaging recipes, just copy packages from conda-forge to plotly channel

--- packaging/conda_build_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-conda build flask-compress/
-conda build dash-renderer/
-conda build dash/
-conda build dash-html-components/
-conda build dash-core-components/
-conda build dash-table/
-conda build dash-daq/
